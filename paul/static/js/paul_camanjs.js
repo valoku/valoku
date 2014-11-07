bc230c11dee5a33914c8e289b2d5169521a0a7a6
Niklas <nyholmniklas@gmail.com>
2014-11-08 00:47:51 +0200

    Oops, the size..

--- paul/static/js/paul_camanjs.js
+++ paul/static/js/paul_camanjs.js
@@ -1,18 +1,7 @@
 window.onload = function () {
     var canvas = document.getElementById('image-canvas').getContext('2d');
     var image_element = document.getElementById('canvas-image-source');
-    canvas.drawImage(image_element, 300, image_element.height * (300/img.width));
-
-
-
-
-
-
-
-
-
-
-
+    canvas.drawImage(image_element, 0, 0);
 
     var bri = document.getElementById('bri')
     /* Function trigerred when we leave the input */
