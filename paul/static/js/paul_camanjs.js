9ce1cca0bb84edaf87d70a9e79c47d681aba1830
Niklas <nyholmniklas@gmail.com>
2014-11-08 01:44:09 +0200

    Image finally resizes properly

--- paul/static/js/paul_camanjs.js
+++ paul/static/js/paul_camanjs.js
@@ -1,31 +1,33 @@
 window.onload = function () {
     var canvasContainer = document.getElementById('canvas-container');
-    var canvas = document.getElementById('image-canvas').getContext('2d');
+    var canvasContext = document.getElementById('image-canvas').getContext('2d');
     var imageElement = document.getElementById('canvas-image-source');
     drawImage();
 
     var bri = document.getElementById('bri')
     /* Function trigerred when we leave the input */
-    bri.onblur = function () {
-        var amount = this.value;
-
-        var img = document.getElementById('img');
-
-        /* We change the brightness of the canvas itself */
-        img.setAttribute('style', 'filter:brightness(' + amount + '); -webkit-filter:brightness(' + amount + '); -moz-filter:brightness(' + amount + ')');
-
-    }
+//    bri.onblur = function () {
+//        var amount = this.value;
+//
+//        var img = document.getElementById('img');
+//
+//        /* We change the brightness of the canvas itself */
+//        img.setAttribute('style', 'filter:brightness(' + amount + '); -webkit-filter:brightness(' + amount + '); -moz-filter:brightness(' + amount + ')');
+//
+//    }
 
     window.onresize = function () {
         drawImage();
     }
 
     function drawImage() {
-        canvas.clearRect (0 , 0 , 9999 , 9999);
-        var canvasWidth = (imageElement.naturalHeight * canvasContainer.clientWidth) / imageElement.naturalWidth;
-        var canvasHeight =(imageElement.naturalHeight * canvasWidth) / imageElement.naturalHeight;
-        canvas.drawImage(imageElement, 0, 0, canvasWidth, canvasHeight);
-        canvas.height = 500;
-        canvas.width = 500;
+        canvasContext.clearRect(0, 0, 9999, 9999);
+        var canvasWidth = canvasContainer.clientWidth;
+        var canvasHeight = imageElement.naturalHeight / (imageElement.naturalWidth / canvasWidth);
+        canvasContainer.width = canvasWidth;
+        canvasContainer.height = canvasHeight;
+        canvasContext.canvas.height = canvasHeight;
+        canvasContext.canvas.width = canvasWidth;
+        canvasContext.drawImage(imageElement, 0, 0, canvasWidth, canvasHeight);
     }
 };
