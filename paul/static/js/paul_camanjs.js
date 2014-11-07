113f4b6432415986038ea7a7521a7f12a8b187eb
Niklas <nyholmniklas@gmail.com>
2014-11-08 01:13:37 +0200

    Image inside canvas resizes

--- paul/static/js/paul_camanjs.js
+++ paul/static/js/paul_camanjs.js
@@ -1,7 +1,8 @@
 window.onload = function () {
+    var canvasContainer = document.getElementById('canvas-container');
     var canvas = document.getElementById('image-canvas').getContext('2d');
-    var image_element = document.getElementById('canvas-image-source');
-    canvas.drawImage(image_element, 0, 0);
+    var imageElement = document.getElementById('canvas-image-source');
+    drawImage();
 
     var bri = document.getElementById('bri')
     /* Function trigerred when we leave the input */
@@ -13,5 +14,18 @@ window.onload = function () {
         /* We change the brightness of the canvas itself */
         img.setAttribute('style', 'filter:brightness(' + amount + '); -webkit-filter:brightness(' + amount + '); -moz-filter:brightness(' + amount + ')');
 
+    }
+
+    window.onresize = function () {
+        drawImage();
+    }
+
+    function drawImage() {
+        canvas.clearRect (0 , 0 , 9999 , 9999);
+        var canvasWidth = (imageElement.naturalHeight * canvasContainer.clientWidth) / imageElement.naturalWidth;
+        var canvasHeight =(imageElement.naturalHeight * canvasWidth) / imageElement.naturalHeight;
+        canvas.drawImage(imageElement, 0, 0, canvasWidth, canvasHeight);
+        canvas.height = 500;
+        canvas.width = 500;
     }
 };
