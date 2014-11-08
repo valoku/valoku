5dd80112d3fe40211adf566cd4f1d59e71634987
Niklas <nyholmniklas@gmail.com>
2014-11-08 18:03:34 +0200

    We got brightness

--- paul/static/js/paul_camanjs.js
+++ paul/static/js/paul_camanjs.js
@@ -1,56 +1,59 @@
-var canvasContainer = document.getElementById('canvas-container');
-var canvasContext = document.getElementById('image-canvas').getContext('2d');
-var imageElement = document.getElementById('canvas-image-source');
-drawImage();
-var brightnessSlider = document.getElementById('brightness-slider');
-var bri = document.getElementById('bri')
-/* Function trigerred when we leave the input */
-//    bri.onblur = function () {
-//        var amount = this.value;
-//
-//        var img = document.getElementById('img');
-//
-//        /* We change the brightness of the canvas itself */
-//        img.setAttribute('style', 'filter:brightness(' + amount + '); -webkit-filter:brightness(' + amount + '); -moz-filter:brightness(' + amount + ')');
-//
-//    }
+var myImage = document.getElementById('canvas-image-source');
 
-var camanFilters = {
-    brightness: 0
-}
-
-brightnessSlider.onchange = function () {
-    var amount = brightnessSlider.value;
-    setBrightness(amount);
-}
-
-function setBrightness(amount) {
-    /* We change the brightness of the canvas itself */
-//        canvasContext.style = 'filter:brightness(' + amount + '); -webkit-filter:brightness(' + amount + '); -moz-filter:brightness(' + amount + ')';
-    camanFilters.brightness = amount;
-    updateImage();
-}
-
-function updateImage() {
-    Caman('#image-canvas', function () {
-        this.revert();
-        this.brightness(camanFilters.brightness);
-        this.render();
-        updating = false;
-    });
-}
-
-window.onresize = function () {
+myImage.onload = function () {
+    var canvasContainer = document.getElementById('canvas-container');
+    var canvasContext = document.getElementById('image-canvas').getContext('2d');
+    var imageElement = document.getElementById('canvas-image-source');
     drawImage();
-}
+//    applyFilters();
 
-function drawImage() {
-    canvasContext.clearRect(0, 0, 9999, 9999);
-    var canvasWidth = canvasContainer.clientWidth;
-    var canvasHeight = imageElement.naturalHeight / (imageElement.naturalWidth / canvasWidth);
-    canvasContainer.width = canvasWidth;
-    canvasContainer.height = canvasHeight;
-    canvasContext.canvas.height = canvasHeight;
-    canvasContext.canvas.width = canvasWidth;
-    canvasContext.drawImage(imageElement, 0, 0, canvasWidth, canvasHeight);
+    var brightnessSlider = document.getElementById('brightness-slider');
+
+    var camanFilters = {
+        brightness: 0
+    }
+
+    brightnessSlider.oninput = Foundation.utils.throttle(brightnessChanged, 700);
+
+
+    function brightnessChanged() {
+        var amount = brightnessSlider.value;
+        setBrightness(amount);
+    }
+
+    function setBrightness(amount) {
+        camanFilters.brightness = amount;
+        drawImage();
+    }
+
+    function drawImage() {
+        updateCanvasSize();
+        Caman(canvasContext.canvas, imageElement.src, function () {
+            this.revert();
+            if (canvasContext.canvas != null) {
+                this.resize({
+                    width: canvasContainer.width,
+                    height: canvasContainer.height
+                });
+            }
+            this.brightness(camanFilters.brightness);
+            this.render();
+        });
+    }
+
+    function update() {
+        updateCanvasSize();
+        drawImage();
+    }
+
+    function updateCanvasSize() {
+        var canvasWidth = canvasContainer.clientWidth;
+        var canvasHeight = imageElement.naturalHeight / (imageElement.naturalWidth / canvasWidth);
+        canvasContainer.width = canvasWidth;
+        canvasContainer.height = canvasHeight;
+        canvasContext.canvas.height = canvasHeight;
+        canvasContext.canvas.width = canvasWidth;
+    }
+
+    window.onresize = Foundation.utils.throttle(update, 700);
 }
