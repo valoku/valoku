25decaf3f5f19adf3fc518b1de0732b8433c6de5
Niklas <nyholmniklas@gmail.com>
2014-11-09 22:43:01 +0200

    Hue and saturation now work

--- paul/static/js/paul_camanjs.js
+++ paul/static/js/paul_camanjs.js
@@ -9,13 +9,15 @@ myImage.onload = function () {
 
     var camanFilters = {
         brightness: 0,
-        contrast: 0
+        contrast: 0,
+        hue: 50,
+        saturation: 0
     }
 
     function drawImage() {
         updateCanvasSize();
         Caman(canvasContext.canvas, imageElement.src, function () {
-            this.revert();
+            this.reset();
             if (canvasContext.canvas != null) {
                 this.resize({
                     width: canvasContainer.width,
@@ -24,6 +26,18 @@ myImage.onload = function () {
             }
             this.brightness(camanFilters.brightness);
             this.contrast(camanFilters.contrast);
+            this.render();
+        });
+    }
+
+    function applyFilters() {
+        updateCanvasSize();
+        Caman(canvasContext.canvas, function () {
+            this.revert(false);
+            this.brightness(camanFilters.brightness);
+            this.contrast(camanFilters.contrast);
+            this.hue(camanFilters.hue);
+            this.saturation(camanFilters.saturation);
             this.render();
         });
     }
@@ -46,32 +60,32 @@ myImage.onload = function () {
 
     var brightnessSlider = document.getElementById('brightness-slider');
     var contrastSlider = document.getElementById('contrast-slider');
-    var saturationSlider = document.getElementById('brightness-slider');
-    var hueSlider = document.getElementById('brightness-slider');
+    var saturationSlider = document.getElementById('saturation-slider');
+    var hueSlider = document.getElementById('hue-slider');
 
 
     brightnessSlider.oninput = Foundation.utils.debounce(brightnessChanged, 500);
     function brightnessChanged() {
         camanFilters.brightness = brightnessSlider.value;
-        drawImage();
+        applyFilters();
     }
 
     contrastSlider.onchange = Foundation.utils.debounce(contrastChanged, 500);
     function contrastChanged() {
         camanFilters.contrast = parseInt(contrastSlider.value);
-        drawImage();
+        applyFilters();
     }
 
-//
-//    saturationSlider.oninput = Foundation.utils.throttle(saturationChanged, 700);
-//    function saturationChanged() {
-//        camanFilters.saturation = saturationSlider.value;
-//        drawImage();
-//    }
-//
-//    hueSlider.oninput = Foundation.utils.throttle(hueChanged, 700);
-//    function hueChanged() {
-//        camanFilters.hue = hueSlider.value;
-//        drawImage();
-//    }
+
+    saturationSlider.oninput = Foundation.utils.throttle(saturationChanged, 700);
+    function saturationChanged() {
+        camanFilters.saturation = saturationSlider.value;
+        applyFilters();
+    }
+
+    hueSlider.oninput = Foundation.utils.throttle(hueChanged, 700);
+    function hueChanged() {
+        camanFilters.hue = hueSlider.value;
+        applyFilters();
+    }
 }
