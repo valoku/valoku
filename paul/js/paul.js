efde78318aa2664b3e7d71e00bcfbb2a4864f536
Niklas Nyholm <niklas.nyholm@procountor.com>
2014-12-02 16:25:45 +0200

    Removed save-to-server related code from paul.js

--- paul/js/paul.js
+++ paul/js/paul.js
@@ -125,55 +125,6 @@ $(window).load(function () {
         });
     }
 
-    /**Start of copy pasted csrf django stuff */
-
-    function csrfSafeMethod(method) {
-        // these HTTP methods do not require CSRF protection
-        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-    }
-
-    function sameOrigin(url) {
-        // test that a given url is a same-origin URL
-        // url could be relative or scheme relative or absolute
-        var host = document.location.host; // host + port
-        var protocol = document.location.protocol;
-        var sr_origin = '//' + host;
-        var origin = protocol + sr_origin;
-        // Allow absolute or scheme relative URLs to same origin
-        return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
-            (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
-            // or any other URL that isn't scheme relative or absolute i.e relative.
-            !(/^(\/\/|http:|https:).*/.test(url));
-    }
-
-
-    /**End of copy pasted csrf django stuff */
-
-    function saveToServer(image) {
-        var csrftoken = $.cookie('csrftoken');
-        $.ajaxSetup({
-            beforeSend: function (xhr, settings) {
-                if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
-                    // Send the token to same-origin, relative URLs only.
-                    // Send the token only if the method warrants CSRF protection
-                    // Using the CSRFToken value acquired earlier
-                    xhr.setRequestHeader("X-CSRFToken", csrftoken);
-                }
-            }
-        });
-        var url = '/save_edited_file/' + $('#hidden-image-id').text()
-        jQuery.ajax({
-            url: url,
-            type: 'POST',
-            data: image,
-            cache: false,
-            processData: false,
-            contentType: false,
-            success: function (data) {
-                console.log(data);
-            }
-        });
-    }
 });
 
 var spinner;
