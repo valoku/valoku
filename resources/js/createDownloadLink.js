7c53f1790b6f38f362bd73988a43ea08bd37942c
Niklas <nyholmniklas@gmail.com>
2014-12-06 19:20:21 +0200

    Refactored saveButtonPressed

---
+++ resources/js/createDownloadLink.js
@@ -0,0 +1,31 @@
+function createDownloadLink(context) {
+    // atob to base64_decode the data-URI
+    var base64_data = context.toBase64('jpeg');
+    var image_data = atob(base64_data.split(',')[1]);
+    // Use typed arrays to convert the binary data to a Blob
+    var arraybuffer = new ArrayBuffer(image_data.length);
+    var view = new Uint8Array(arraybuffer);
+    for (var i = 0; i < image_data.length; i++) {
+        view[i] = image_data.charCodeAt(i) & 0xff;
+    }
+    try {
+        // This is the recommended method:
+        var blob = new Blob([arraybuffer], {type: 'application/octet-stream'});
+    } catch (e) {
+        // The BlobBuilder API has been deprecated in favour of Blob, but older
+        // browsers don't know about the Blob constructor
+        // IE10 also supports BlobBuilder, but since the `Blob` constructor
+        //  also works, there's no need to add `MSBlobBuilder`.
+        var bb = new (window.WebKitBlobBuilder || window.MozBlobBuilder);
+        bb.append(arraybuffer);
+        var blob = bb.getBlob('application/octet-stream'); // <-- Here's the Blob
+    }
+    // Use the URL object to create a temporary URL
+    var url = (window.webkitURL || window.URL).createObjectURL(blob);
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    a.href = url;
+    a.download = "file.jpeg";
+    return a;
+}
--- resources/js/valoku.js
+++ resources/js/valoku.js
@@ -111,52 +111,12 @@ function showImage() {
 
     function saveButtonPressed() {
         showLoadingSpinner();
-        //We need to clone the image element and later add it
-        //to the DOM because the call to Caman's render()
-        //replaces the img element with canvas
         var imageElementClone = imageElement.cloneNode(true)
-        Caman(imageElement, function () {
+        Caman(imageElementClone, function () {
             setContextFilters(this);
             this.render(function () {
-                // atob to base64_decode the data-URI
-                var base64_data = this.toBase64('jpeg');
-                var image_data = atob(base64_data.split(',')[1]);
-                // Use typed arrays to convert the binary data to a Blob
-                var arraybuffer = new ArrayBuffer(image_data.length);
-                var view = new Uint8Array(arraybuffer);
-                for (var i = 0; i < image_data.length; i++) {
-                    view[i] = image_data.charCodeAt(i) & 0xff;
-                }
-                try {
-                    // This is the recommended method:
-                    var blob = new Blob([arraybuffer], {type: 'application/octet-stream'});
-                } catch (e) {
-                    // The BlobBuilder API has been deprecated in favour of Blob, but older
-                    // browsers don't know about the Blob constructor
-                    // IE10 also supports BlobBuilder, but since the `Blob` constructor
-                    //  also works, there's no need to add `MSBlobBuilder`.
-                    var bb = new (window.WebKitBlobBuilder || window.MozBlobBuilder);
-                    bb.append(arraybuffer);
-                    var blob = bb.getBlob('application/octet-stream'); // <-- Here's the Blob
-                }
-
-                // Use the URL object to create a temporary URL
-                var url = (window.webkitURL || window.URL).createObjectURL(blob);
-                //location.href = url; // <-- Download!
-                var a = document.createElement("a");
-                document.body.appendChild(a);
-                a.style = "display: none";
-                a.href = url;
-                a.download = "file.jpeg";
+                var a = createDownloadLink(this);
                 a.click();
-
-
-                //We now remove the canvas element that took the place of the img
-                imageElement = document.getElementById("canvas-image-source");
-                imageElement.parentNode.removeChild(imageElement);
-                //Then we add the clone to the DOM and assign it to imageElement
-                document.body.appendChild(imageElementClone);
-                imageElement = imageElementClone;
                 hideLoadingSpinner();
             });
         });
